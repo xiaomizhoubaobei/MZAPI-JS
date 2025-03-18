9ba2d8c2add8cf8fce66055f6f8231ee8ca059b1
祁筱欣 <qixiaoxin@stu.sqxy.edu.cn>
2025-03-19 00:54:03 +0800

    refactor(sdk): 重构 SDK 以支持 TypeScript
    - 将项目从 JavaScript 重构为 TypeScript- 更新了文件结构和命名规范
    - 优化了代码的类型定义和接口设计
    - 移除了不必要的依赖
    - 新增了 TypeScript 相关的配置文件

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * MZAPI SDK
+ *
+ * 这是一个基于JavaScript的SDK，旨在帮助开发者快速接入各种API服务。
+ * 目前主要提供百度文心大模型API的接入功能。
+ *
+ * @packageDocumentation
+ */
+
+// 导出具体模型实现
+export {default as Ernie4_8K} from './MB/ERNIE/4.0-8K';
+export {default as Ernie4_8K_Preview} from './MB/ERNIE/4.0-8K-Preview';
--- src/main.js
+++
@@ -1,3 +0,0 @@
-const Ernie4_8K = require('./MB/ERNIE/4.0-8K');
-
-module.exports = Ernie4_8K;
